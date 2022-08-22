fc8eda2d5f6c2d78d72dac0ad5a43643d71d6f84
Arth Tyagi <arthtyagi7@gmail.com>
2022-08-22 21:42:35 +0530

    refactor code; call dispatch after successful response

--- src/components/Settings.tsx
+++ src/components/Settings.tsx
@@ -2,8 +2,9 @@ import React from 'react';
 import {
   Formik, Form, Field, ErrorMessage,
 } from 'formik';
+import { useMutation, QueryClient } from '@tanstack/react-query';
 import axios from 'axios';
-import { useUserContext } from '../services/userContext';
+import { useUserContext, useUserDispatch } from '../services/userContext';
 
 const API_URL = import.meta.env.VITE_API_URL;
 
@@ -41,6 +42,8 @@ export async function updateUser(first_name?: string, last_name?: string, userna
 
 function Settings(): JSX.Element {
   const loggedInUser = useUserContext();
+  const { pk, email } = loggedInUser;
+  const dispatch = useUserDispatch();
   const validate = (values: UpdateUserValues) => {
     const errors: UpdateUserValues['errors'] = {};
     if (!values.firstName) {
@@ -57,18 +60,25 @@ function Settings(): JSX.Element {
   const handleSubmit = async (values: UpdateUserValues, { setSubmitting }: any) => {
     try {
       setSubmitting(true);
-      const { firstName, lastName, username } = values;
-      if (username !== loggedInUser.username) {
-        await updateUser(firstName, lastName, username)
-          .then(() => {
-            localStorage.removeItem('loggedInUser');
-          });
-      } else {
-        await updateUser(firstName, lastName)
-          .then(() => {
-            localStorage.removeItem('loggedInUser');
-          });
+      const { firstName, lastName } = values;
+      let { username } = values;
+      if (username === loggedInUser.username) {
+        username = undefined;
       }
+      await updateUser(firstName, lastName, username)
+        .then(() => {
+          dispatch({
+            type: 'SET_USER_DATA',
+            payload: {
+              pk,
+              username,
+              email,
+              first_name: firstName,
+              last_name: lastName,
+            },
+          });
+          localStorage.removeItem('loggedInUser');
+        });
     } catch (error) {
       setSubmitting(false);
     }
