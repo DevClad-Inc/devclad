2fcc9c863257b1b5bf2195d99f59c58752b99e88
Arth Tyagi <arthtyagi7@gmail.com>
2022-10-17 03:38:06 +0530

    refactor validate organizer; decrease working memory, make more readable and simpler

--- apps/server/social/meetings/serializers.py
+++ apps/server/social/meetings/serializers.py
@@ -14,15 +14,14 @@ class MeetingSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = MeetingRoom
-        exclude = ["attended"]
+        read_only_fields = ["attended"]
 
     def get_invites(self, obj):
         meeting = MeetingRoom.objects.get(uid=obj.uid)
         return meeting.get_flat_values("invites")
 
     def validate_organizer(self, value):
-        is_self = value == User.objects.get(username=self.context["request"].user)
-        match is_self:
+        match (value == User.objects.get(username=self.context["request"].user)):
             case True:
                 return value
             case _:
@@ -31,33 +30,33 @@ class MeetingSerializer(serializers.ModelSerializer):
                 )
 
     def validate_invites(self, value):
-        if self.context["request"].user.id not in value["invites"]:
-            self.context["request"].data["invites"].append(
-                self.context["request"].user.id
-            )
-        if len(value["invites"]) > 2:
-            raise serializers.ValidationError(
-                "You can only invite 2 people to a meeting"
-            )
-        for invite in value["invites"]:
-            match_or_circle = (
-                invite
-                in self.context["request"]
-                .user.socialprofile.matches_this_week.all()
-                .values_list("id", flat=True)
-                or invite
-                in self.context["request"]
-                .user.socialprofile.circle_symmetrical.all()
-                .values_list("id", flat=True)
-                or invite == self.context["request"].user.id
-            )
-            match match_or_circle:
-                case False:
-                    raise serializers.ValidationError(
-                        "User is not in your circle or matches this week."
-                    )
-                case _:
-                    pass
+        match (len(value["invites"]) == 1):
+            case True:
+                invite = value["invites"][0]
+                is_self = invite == self.context["request"].user.id
+                is_in_circle = invite in self.context[
+                    "request"
+                ].user.socialprofile.circle_symmetrical.all().values_list(
+                    "id", flat=True
+                )
+                is_match = invite in self.context[
+                    "request"
+                ].user.socialprofile.matches_this_week.all().values_list(
+                    "id", flat=True
+                )
+                match (is_in_circle or is_match or is_self):
+                    case True:
+                        self.context["request"].data["invites"].append(
+                            self.context["request"].user.id
+                        )
+                    case _:
+                        raise serializers.ValidationError(
+                            "User is not in your circle or matches this week."
+                        )
+            case _:
+                raise serializers.ValidationError(
+                    "Maximum number of invites is 1 for this meeting type"
+                )
         return value
 
     def validate_type_of(self, value):
