2bd79e7b8451694e21f07253b5c8dba36b1c59b2
Arth Tyagi <arthtyagi7@gmail.com>
2022-10-28 22:59:30 +0530

    audiences beta

--- apps/landing/pages/_app.tsx
+++ apps/landing/pages/_app.tsx
@@ -1,8 +1,14 @@
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
+import { Analytics } from '@vercel/analytics/react';
 import '@devclad/ui/fontscss';
 
 export default function App({ Component, pageProps }: AppProps) {
-	// eslint-disable-next-line react/jsx-props-no-spreading
-	return <Component {...pageProps} />;
+	return (
+		<>
+			{/* eslint-disable-next-line react/jsx-props-no-spreading */}
+			<Component {...pageProps} />;
+			<Analytics />
+		</>
+	);
 }
