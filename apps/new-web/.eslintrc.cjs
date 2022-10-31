abb38e1cc40733ec9592ea6fe1fbd4a946f90a3e
Arth Tyagi <arthtyagi7@gmail.com>
2022-10-31 14:57:48 +0530

    nextjs migration

---
+++ apps/new-web/.eslintrc.cjs
@@ -0,0 +1,22 @@
+module.exports = {
+	...require('@devclad/config/eslint-react'),
+	extends: ['next/core-web-vitals'],
+	ignorePatterns: [
+		'**/*.js',
+		'**/*.cjs',
+		'**/*.json',
+		'node_modules',
+		'public',
+		'dist',
+		'vite.config.ts',
+	],
+	parserOptions: {
+		ecmaFeatures: {
+			jsx: true,
+		},
+		ecmaVersion: 'latest',
+		sourceType: 'module',
+		project: 'tsconfig.json',
+		tsconfigRootDir: __dirname,
+	},
+};
