aa165821f3e138f293cc04510cc0edd663c538aa
Arth Tyagi <arthtyagi7@gmail.com>
2022-11-06 12:36:25 +0530

    update landing page

--- apps/landing/components/Hero.tsx
+++ apps/landing/components/Hero.tsx
@@ -36,6 +36,9 @@ export default function Hero(): JSX.Element {
 						<div className="lg:py-24">
 							<h1 className="mt-5 text-5xl font-black tracking-tighter text-white lg:-mt-10">
 								DevClad
+								<span className="text-base font-medium tracking-normal text-orange-500">
+									beta
+								</span>
 							</h1>
 							<span className="mb-2 flex sm:justify-center">
 								<h1 className="mt-1 w-fit font-mono text-xl font-bold text-orange-50">
@@ -46,22 +49,55 @@ export default function Hero(): JSX.Element {
 									</span>
 								</h1>
 							</span>
-							<a
-								href="https://discord.connectdome.com"
-								target="_blank"
-								className="mb-10 inline-flex items-center rounded-full border-[1px]
-               border-neutral-900 bg-black p-1 pr-2 sm:text-base lg:text-sm xl:text-base"
-								rel="noreferrer"
-							>
-								<span className="rounded-full border-[0.5px] border-neutral-500 px-4 py-0.5 font-mono text-xs font-semibold text-orange-200 lg:text-sm">
-									Beta 1.0
-								</span>
-								<span className="ml-4 text-xs lg:text-sm">Join our Discord</span>
-								<ChevronRightIcon
-									className="ml-2 h-5 w-5 text-neutral-500"
-									aria-hidden="true"
-								/>
-							</a>
+							<span className="mb-2 mt-2 flex sm:justify-center">
+								<a
+									href="https://github.com/devclad-inc/devclad/"
+									target="_blank"
+									className="mb-2 inline-flex items-center rounded-lg border-[1px] border-dashed border-neutral-700 bg-black
+               p-1 pr-2 sm:rounded-full sm:text-base lg:text-sm xl:text-base"
+									rel="noreferrer"
+								>
+									<span className="ml-4">
+										<svg
+											className="mr-2 h-6 w-4 sm:h-8 sm:w-5"
+											aria-hidden="true"
+											fill="currentColor"
+											viewBox="0 0 20 20"
+										>
+											<path
+												fillRule="evenodd"
+												d="M10 0C4.477 0 0 4.484 0 10.017c0 4.425 2.865 8.18 6.839 9.504.5.092.682-.217.682-.483 0-.237-.008-.868-.013-1.703-2.782.605-3.369-1.343-3.369-1.343-.454-1.158-1.11-1.466-1.11-1.466-.908-.62.069-.608.069-.608 1.003.07 1.531 1.032 1.531 1.032.892 1.53 2.341 1.088 2.91.832.092-.647.35-1.088.636-1.338-2.22-.253-4.555-1.113-4.555-4.951 0-1.093.39-1.988 1.029-2.688-.103-.253-.446-1.272.098-2.65 0 0 .84-.27 2.75 1.026A9.564 9.564 0 0110 4.844c.85.004 1.705.115 2.504.337 1.909-1.296 2.747-1.027 2.747-1.027.546 1.379.203 2.398.1 2.651.64.7 1.028 1.595 1.028 2.688 0 3.848-2.339 4.695-4.566 4.942.359.31.678.921.678 1.856 0 1.338-.012 2.419-.012 2.747 0 .268.18.58.688.482A10.019 10.019 0 0020 10.017C20 4.484 15.522 0 10 0z"
+												clipRule="evenodd"
+											/>
+										</svg>
+									</span>
+									<span className=" text-xs lg:text-sm">
+										Open-Sourced on GitHub
+									</span>
+									<ChevronRightIcon
+										className="ml-2 h-5 w-5 text-neutral-500"
+										aria-hidden="true"
+									/>
+								</a>
+							</span>
+							<span className="mb-2 mt-2 flex sm:justify-center ">
+								<a
+									href="https://discord.connectdome.com"
+									target="_blank"
+									className="inline-flex items-center rounded-lg  border-[1px] border-neutral-800 bg-black p-1
+               pr-2 sm:mt-0 sm:rounded-full sm:text-base lg:text-sm xl:text-base"
+									rel="noreferrer"
+								>
+									<span className="ml-4 text-xs lg:text-sm">
+										Join our Discord
+									</span>
+									<ChevronRightIcon
+										className="ml-2 h-5 w-5 text-neutral-500"
+										aria-hidden="true"
+									/>
+								</a>
+							</span>
+
 							<div className="space-y-4">
 								<div className="flex text-center sm:justify-center">
 									<div
