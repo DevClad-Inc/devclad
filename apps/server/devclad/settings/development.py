1e8cd0abea72e8ec886ba5deb1565fd58ee6ccea
Arth Tyagi <arthtyagi7@gmail.com>
2022-11-24 16:03:16 +0530

    s3v4

--- apps/server/devclad/settings/development.py
+++ apps/server/devclad/settings/development.py
@@ -25,7 +25,7 @@ AWS_ACCESS_KEY_ID = config("AWS_ACCESS_KEY_ID", default="", cast=str)
 AWS_SECRET_ACCESS_KEY = config("AWS_SECRET_ACCESS_KEY", default="", cast=str)
 AWS_STORAGE_BUCKET_NAME = "devclad"  # hc
 AWS_S3_REGION_NAME = "us-east-1"
-# AWS_S3_SIGNATURE_VERSION = 's3v4'
+AWS_S3_SIGNATURE_VERSION = "s3v4"
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = "public-read"
