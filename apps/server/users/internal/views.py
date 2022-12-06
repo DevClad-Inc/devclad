7b2dc36ce68cdec7cce13b6e93f5ba8ece4a478c
Arth Tyagi <arthtyagi7@gmail.com>
2022-12-06 15:31:45 +0530

    fix exclude

--- apps/server/users/internal/views.py
+++ apps/server/users/internal/views.py
@@ -22,9 +22,9 @@ def manage_users(request: Request) -> Response:
     match request.method:
         case "GET":
             if status := request.query_params.get("status"):
-                user_statuses = UserStatus.objects.exclude(
-                    status="Not Submitted"
-                ).filter(approved=status)
+                user_statuses = UserStatus.objects.exclude(status="Submitted").filter(
+                    approved=status
+                )
                 users = [user_status.user for user_status in user_statuses]
                 status_serializer = UserStatusSerializer(user_statuses, many=True)
                 user_serializer = UserSerializer(users, many=True)
