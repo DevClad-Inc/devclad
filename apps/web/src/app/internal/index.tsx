30ee7949242053b8b8900623e74e02b7e977d664
Arth Tyagi <arthtyagi7@gmail.com>
2023-01-17 14:55:56 +0530

    improve internal view

--- apps/web/src/app/internal/index.tsx
+++ apps/web/src/app/internal/index.tsx
@@ -80,73 +80,124 @@ export default function Internal() {
 
 	if (isSuccess && data !== null) {
 		return (
-			<ul className="mt-5 grid grid-cols-1 gap-6 sm:grid-cols-2">
-				{data.data.users.map((user: ManagedUser) => (
-					<li
-						key={user.id}
-						className="bg-darkBG2 col-span-1 divide-y divide-neutral-800 rounded-md border-[1px] border-neutral-800 shadow-2xl shadow-white/5"
-					>
-						<div className="flex w-full items-center justify-between space-x-6 p-6">
-							<div className="flex-1 truncate">
-								<div className="flex items-center space-x-3">
-									<h3 className="truncate text-sm font-medium text-neutral-100">
-										{user.first_name}
+			<div className="bg-darkBG1 min-h-screen">
+				<div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+					<div className="px-4 py-6 sm:px-0">
+						<div className="h-96 rounded-lg border-4 border-dashed border-neutral-800">
+							<div className="flex h-full flex-col items-center justify-center">
+								<div className="text-center">
+									<CheckCircleIcon
+										className="mx-auto h-12 w-12 text-neutral-400"
+										aria-hidden="true"
+									/>
+									<h3 className="mt-2 text-lg font-medium text-neutral-100">
+										{data.data.users.length} Users
 									</h3>
-									<span className="bg-phthaloGreen text-honeyDew inline-block flex-shrink-0 rounded-full px-2 py-0.5 text-xs font-medium">
-										{user.username}
-									</span>
-									<span className="text-honeyDew inline-block flex-shrink-0 rounded-full bg-cyan-800 px-2 py-0.5 text-xs font-medium">
-										<Link to={`/profile/${user.username}`}> View Profile</Link>
-									</span>
-								</div>
-
-								<p className="mt-2 truncate text-sm text-neutral-500">
-									{user.email}
-								</p>
-							</div>
-						</div>
-						<div>
-							{user.email && (
-								<div className="-mt-px flex divide-x divide-neutral-800">
-									<div className="flex w-0 flex-1">
-										<button
-											type="button"
-											onClick={() => {
-												handleMail(user.first_name, user.email, 'reminder');
-											}}
-											className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-md border border-transparent py-4 text-sm font-medium text-neutral-300 hover:text-neutral-100"
-										>
-											<ClockIcon className="h-5 w-5" aria-hidden="true" />
-											<span className="ml-3">Remind</span>
-										</button>
-									</div>
-
-									<div className="-ml-px flex w-0 flex-1">
+									<p className="mt-1 text-sm text-neutral-500">
+										{queryParams.get('status') === 'True'
+											? 'Approved Users'
+											: 'Pending Users'}
+									</p>
+									<div className="mt-6">
 										<PrimaryButton
 											onClick={() => {
 												handleMail(
-													user.first_name,
-													user.email,
+													'',
+													'',
 													'approved',
-													user.id,
-													'True'
+													0,
+													queryParams.get('status') === 'True'
+														? 'False'
+														: 'True'
 												);
 											}}
-											className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-md border border-transparent py-4 text-sm font-medium text-neutral-300 hover:text-neutral-100"
 										>
-											<CheckCircleIcon
-												className="h-5 w-5"
-												aria-hidden="true"
-											/>
-											<span className="ml-3">Approve</span>
+											{queryParams.get('status') === 'True'
+												? 'Send Unapproved Emails'
+												: 'Send Approved Emails'}
 										</PrimaryButton>
 									</div>
 								</div>
-							)}
+							</div>
 						</div>
-					</li>
-				))}
-			</ul>
+					</div>
+				</div>
+
+				<ul className="mt-5 grid grid-cols-2 gap-6 sm:grid-cols-3">
+					{data.data.users.map((user: ManagedUser) => (
+						<li
+							key={user.id}
+							className="bg-darkBG2 col-span-1 divide-y divide-neutral-800 rounded-md border-[1px] border-neutral-800 shadow-2xl shadow-white/5"
+						>
+							<div className="flex w-full items-center justify-between space-x-6 p-6">
+								<div className="flex-1 truncate">
+									<div className="flex items-center space-x-3">
+										<h3 className="truncate text-sm font-medium text-neutral-100">
+											{user.first_name}
+										</h3>
+										<span className="bg-phthaloGreen text-honeyDew inline-block flex-shrink-0 rounded-full px-2 py-0.5 text-xs font-medium">
+											{user.username}
+										</span>
+										<span className="text-honeyDew inline-block flex-shrink-0 rounded-full bg-cyan-800 px-2 py-0.5 text-xs font-medium">
+											<Link to={`/profile/${user.username}`}>
+												{' '}
+												View Profile
+											</Link>
+										</span>
+									</div>
+
+									<p className="mt-2 truncate text-sm text-neutral-500">
+										{user.email}
+									</p>
+								</div>
+							</div>
+							<div>
+								{user.email && (
+									<div className="-mt-px flex divide-x divide-neutral-800">
+										<div className="flex w-0 flex-1">
+											<button
+												type="button"
+												onClick={() => {
+													handleMail(
+														user.first_name,
+														user.email,
+														'reminder'
+													);
+												}}
+												className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center rounded-bl-md border border-transparent py-4 text-sm font-medium text-neutral-300 hover:text-neutral-100"
+											>
+												<ClockIcon className="h-5 w-5" aria-hidden="true" />
+												<span className="ml-3">Remind</span>
+											</button>
+										</div>
+
+										<div className="-ml-px flex w-0 flex-1">
+											<PrimaryButton
+												onClick={() => {
+													handleMail(
+														user.first_name,
+														user.email,
+														'approved',
+														user.id,
+														'True'
+													);
+												}}
+												className="relative inline-flex w-0 flex-1 items-center justify-center rounded-br-md border border-transparent py-4 text-sm font-medium text-neutral-300 hover:text-neutral-100"
+											>
+												<CheckCircleIcon
+													className="h-5 w-5"
+													aria-hidden="true"
+												/>
+												<span className="ml-3">Approve</span>
+											</PrimaryButton>
+										</div>
+									</div>
+								)}
+							</div>
+						</li>
+					))}
+				</ul>
+			</div>
 		);
 	}
 	return <div>Internal not found</div>;
